dc2a0747b671f3fd51429eea9a8bc49a276ba56e
Andrew Darqui <andrew.darqui@gmail.com>
2016-11-17 16:29:56 -0500

    usage

--- misc.c
+++ misc.c
@@ -5,10 +5,19 @@ void usage(void)
 {
 	errx(1,
 	     "usage: ./watchque [<redishost:port[:redisdb]>|</path/to/bin/dir>] <metadata> <Class1>:<Queue1>:<Events>:<Directory1,...,DirectoryN> ... <ClassN>:<QueueN>:<Events>:<Directory1, ...,DirectoryN>\n" \
+         "\n" \
+         "* Events:\n" \
+         "  c - Create\n" \
+         "  C - Close write\n" \
+         "  u - Update\n" \
+         "  d - Delete\n" \
+         "  r - Rename\n" \
+         "\n" \
          "* flags:\n" \
          "  --debug=N where N = debug level\n" \
          "  --meta=<metadata string>\n" \
          "  --post=<rpush,lpush,publish>\n" \
+         "\n" \
          "VERSION: %s\n" \
          "EOF"
     , VERSION);
